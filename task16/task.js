9a3012fdef6eb4e0adf464484819f385afaad9b5
supertiny99 <309693441@qq.com>
2016-03-23 21:49:37 +0800

    Updata

--- task16/task.js
+++ task16/task.js
@@ -7,7 +7,6 @@
  * };
  */
 var aqiData = {};
-//var aqiData = [];
 /**
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
@@ -15,50 +14,36 @@ var aqiData = {};
 function addAqiData() {
 	var cityInput = document.getElementById("aqi-city-input").value.trim();
 	var valueInput = document.getElementById("aqi-value-input").value.trim();
-	var reg1 =/^[\u4e00-\u9f5a]*[a-z]*[A-Z]*$/;
-	var reg2 = /^[0-9]+$/;
+	var reg1 =/^[a-zA-Z\u4e00-\u9f5a]+$/;
+	var reg2 = /^(0|[1-9]\d*)$/;
 	var result1 = cityInput.search(reg1);
 	var result2 = valueInput.search(reg2);
-	//console.log(result1);
-	//console.log(result2);
 	if(result1!=-1 && result2!=-1){
-		//aqiData.push([cityInput, valueInput]);
 		aqiData[cityInput] = parseInt(valueInput);
 	}else if(result2 == -1){
 		alert("空气质量指数输入有误！");
 	}else if(result1 == -1){
 		alert("城市名称有误");
 	}
-	//console.log(aqiData);
 }
 
 /**
  * 渲染aqi-table表格
  */
 function renderAqiList() {
+	//清空表格
 	document.getElementById("aqi-table").innerHTML = "";
+	//创建表头
 	var ths = ["城市","空气质量","操作"];
 	var fragment = document.createDocumentFragment();
 	var tr = document.createElement("tr");
 	for(var i=0; i<3; i++){
-		var td = document.createElement("td");
-		td.innerHTML = ths[i];
-		tr.appendChild(td);
+		var th = document.createElement("th");
+		th.innerHTML = ths[i];
+		tr.appendChild(th);
 	}
 	fragment.appendChild(tr);
-	/*for(var r=0; r<aqiData.length; r++){
-		var tr = document.createElement("tr");
-		for(var c=0; c<2; c++){
-			var td = document.createElement("td");
-			td.innerHTML = aqiData[r][c];
-			tr.appendChild(td);
-		}
-		var td = document.createElement("td");
-		var btn = document.createElement("button");
-		btn.innerHTML = "删除";
-		tr.appendChild(btn);
-		fragment.appendChild(tr);
-	}*/
+	//通过aqiData对象创建表格内容
 	for(var key in aqiData){
 		var tr = document.createElement("tr");
 		var td1 = document.createElement("td");
@@ -68,6 +53,7 @@ function renderAqiList() {
 		td2.innerHTML = aqiData[key];
 		tr.appendChild(td2);
 		var td3 = document.createElement("td");
+		//创建按钮
 		var btn = document.createElement("button");
 		btn.innerHTML = "删除";
 		btn.onclick = function(){
@@ -97,9 +83,10 @@ function addBtnHandle() {
  */
 function delBtnHandle(btn) {
   // do sth.
-  var city = btn.parentNode.parentNode.firstElementChild.innerHTML;
+  var trNode = btn.parentNode.parentNode;
+  var city = trNode.firstElementChild.innerHTML;
   delete aqiData[city];
-  renderAqiList();
+  trNode.parentNode.removeChild(trNode);
 }
 
 function init() {
